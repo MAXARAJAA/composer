3b6a8096c7392f42a00024f7a0317b79db72506a
MAXARAJAA <114005737+MAXARAJAA@users.noreply.github.com>
2023-03-27 09:14:13 +0300

    Add files via upload

---
+++ composer/composer/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace composer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var jewelryBox = new JewelryBox { Name = "Моя шкатулка з прекрасами" };
+            var topTray = new Tray { Name = "Верхній відсік" };
+            //ringCompartment.AddContent("сапфірна прекраса");
+            var ringCompartment = new Compartment { Name = "Нижній відсік" };
+
+            ringCompartment.AddContent("золоте кільце");
+            ringCompartment.AddContent("срібряне кільце");
+            ringCompartment.AddContent("сапфірна прекраса");
+            topTray.Add(ringCompartment);
+            jewelryBox.Add(topTray);
+            jewelryBox.Display(0, textBox1);
+
+
+
+        }
+    }
+}
